c3f34838af086419c1acf46d86e472dd6bf0a916
rodrigonm <rdrg.n.magalhaes@gmail.com>
2018-04-08 19:25:53 -0300

    Setup real time message

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,8 @@ class CommentsController < ApplicationController
     else
       @comment = @article.comments.build(comment_params) @comment.user = current_user
       if @comment.save
+        ActionCable.server.broadcast "comments",
+        render(partial: 'comments/comment', object: @comment )
         flash[:notice] = "Comment has been created"
       else
         flash.now[:alert] = "Comment has not been created"
